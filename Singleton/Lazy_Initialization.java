39953d9e9c2a5f0574813b1a0672082999fc78ee
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-03-26 14:27:15 +0700

    Паттерн проектирования Singleton (Lazy_Initialization ленивая инициализация)

---
+++ Singleton/Lazy_Initialization.java
@@ -0,0 +1,23 @@
+package Singleton;
+
+public class Lazy_Initialization {
+    private static Lazy_Initialization INSTANCE;
+
+    private Lazy_Initialization() {}
+
+    public static Lazy_Initialization getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Lazy_Initialization();
+        }
+        return INSTANCE;
+    }
+
+    //Плюсы:
+    //Ленивая инициализация.
+
+    //Минусы:
+    //Не потокобезопасно
+
+    //Реализация интересна. Мы можем инициализироваться лениво, но утратили потокобезопасность.
+    //Не беда: в реализации номер три мы все синхронизируем.
+}
