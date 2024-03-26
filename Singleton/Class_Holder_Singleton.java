1b413a16e2feba5464e11acd9278fe8a6b7ede37
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-03-26 14:32:01 +0700

    Паттерн проектирования Singleton (Class_Holder_Singleton)

---
+++ Singleton/Class_Holder_Singleton.java
@@ -0,0 +1,28 @@
+package Singleton;
+
+public class Class_Holder_Singleton {
+    private Class_Holder_Singleton() {
+    }
+
+    private static class SingletonHolder {
+        public static final Class_Holder_Singleton HOLDER_INSTANCE = new Class_Holder_Singleton();
+    }
+
+    public static Class_Holder_Singleton getInstance() {
+        return SingletonHolder.HOLDER_INSTANCE;
+    }
+    //Плюсы:
+    //Ленивая инициализация.
+
+    //Потокобезопасность.
+
+    //Высокая производительность в многопоточной среде.
+
+    //Минусы:
+    //Для корректной работы необходима гарантия, что объект класса Singleton инициализируется без ошибок.
+    //Иначе первый вызов метода getInstance закончится ошибкой ExceptionInInitializerError,
+    //а все последующие NoClassDefFoundError.
+
+    //Реализация практически идеальная. И ленивая, и потокобезопасная, и быстрая.
+    //Но есть нюанс, описанный в минусе.
+}
